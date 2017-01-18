cb5f7ab82841300cdd3051054f91bf80bdf75761
Stersh <siper13@gmail.com>
2017-01-18 22:28:53 +0300

    New search library

--- app/src/main/java/pro/siper/myzuka/MainActivity.java
+++ app/src/main/java/pro/siper/myzuka/MainActivity.java
@@ -3,46 +3,37 @@ package pro.siper.myzuka;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.SearchView;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.miguelcatalan.materialsearchview.MaterialSearchView;
+import com.arlib.floatingsearchview.FloatingSearchView;
+import com.arlib.floatingsearchview.suggestions.model.SearchSuggestion;
 import com.yqritc.recyclerviewflexibledivider.HorizontalDividerItemDecoration;
 
-import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import java.io.IOException;
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements MaterialSearchView.OnQueryTextListener {
+public class MainActivity extends AppCompatActivity
+        implements FloatingSearchView.OnQueryChangeListener, FloatingSearchView.OnSearchListener {
     final String TAG = "MainActivity";
     ArrayList<Song> songs = new ArrayList<>();
     RecyclerView songsList;
-    MaterialSearchView searchView;
+    FloatingSearchView floatingSearchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        searchView = (MaterialSearchView) findViewById(R.id.search_view);
-
         songsList = (RecyclerView) findViewById(R.id.songs_list);
+        floatingSearchView = (FloatingSearchView) findViewById(R.id.floating_search_view);
+        floatingSearchView.setOnQueryChangeListener(this);
+        floatingSearchView.setOnSearchListener(this);
 
         final String path = "/";
         SongsAdapter adapter = new SongsAdapter(songs, new AdapterCallbacks() {
@@ -89,19 +80,14 @@ public class MainActivity extends AppCompatActivity implements MaterialSearchVie
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        MenuItem searchItem = menu.findItem(R.id.search);
-        searchView.setMenuItem(searchItem);
-        searchView.setOnQueryTextListener(this);
+    public void onSearchTextChanged(String oldQuery, String newQuery) {
 
-        return true;
     }
 
     @Override
-    public boolean onQueryTextSubmit(String query) {
-        Log.d(TAG, "Submit");
-        if(query.length() > 0) {
+    public void onSearchAction(String currentQuery) {
+        floatingSearchView.showProgress();
+        if(currentQuery.length() > 3) {
             new LoadData(new OnTaskCompleted() {
                 @Override
                 public void onTaskCompleted(Document doc) {
@@ -117,29 +103,15 @@ public class MainActivity extends AppCompatActivity implements MaterialSearchVie
                                 cols.get(1).select("a").text(), cols.get(1).select("a").attr("href")));
                         songsList.getAdapter().notifyItemInserted(i);
                     }
+                    floatingSearchView.hideProgress();
                 }
-            }).execute("https://myzuka.fm/Search?searchText=" + query);
-        } else {
-            new LoadData(new OnTaskCompleted() {
-                @Override
-                public void onTaskCompleted(Document doc) {
-                    Elements elements = doc.getElementsByAttribute("data-url");
-                    songs.clear();
-                    for(int i = 0; i < elements.size(); i++) {
-                        Element element = elements.get(i);
-                        songs.add(new Song(element.attr("data-title"), element.attr("data-url")));
-                        songsList.getAdapter().notifyItemInserted(i);
-                    }
-                }
-            }).execute("https://myzuka.fm/");
+            }).execute("https://myzuka.fm/Search?searchText=" + currentQuery);
         }
-        return true;
     }
 
     @Override
-    public boolean onQueryTextChange(String newText) {
-        Log.d(TAG, "Change");
-        return false;
+    public void onSuggestionClicked(SearchSuggestion searchSuggestion) {
+
     }
 
     @Override
